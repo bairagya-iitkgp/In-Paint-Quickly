5e7eca119fed31e5554b64f519151e62c430c85f
bairagya-iitkgp <43261389+bairagya-iitkgp@users.noreply.github.com>
2018-12-16 19:46:06 +0530

    Add files via upload

---
+++ data_256/to_npy.py
@@ -0,0 +1,29 @@
+import glob
+import os
+import cv2
+import numpy as np
+
+ratio = 0.9
+image_size = 256
+
+x = []
+paths = glob.glob('./original/*')
+for path in paths:
+    img = cv2.imread(path)
+    img = cv2.resize(img, (256, 256))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    x.append(img)
+
+x = np.array(x, dtype=np.uint8)
+np.random.shuffle(x)
+
+p = int(ratio * len(x))
+x_train = x[:p]
+x_test = x[p:]
+
+if not os.path.exists('./npy'):
+    os.mkdir('./npy')
+np.save('./npy/x_train.npy', x_train)
+np.save('./npy/x_test.npy', x_test)
+   
+
